8d3efd42552720ee2b3705c8296acd6b62401260
katie frances <katiefrances@protonmail.com>
2019-08-03 12:48:39 +1000

    Fix xs to sm breakpoint scrolling

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-var offset = 56;
+let offset = 56;
 
 $(".navbar li a").click(function(event) {
   event.preventDefault();
   $($(this).attr("href"))[0].scrollIntoView();
   scrollBy(0, -offset);
 });
+
+// let hideShow = () => {
+//   let show = document.getElementById("nav-link a");
+//   if (show.style.display === "none") {
+//     show.style.display = "block";
+//   } else {
+//     show.style.display = "none";
+//   }
+// };
